7987152c258aa31c57f0b173930060dccd8b6652
rowl <airowl.gasga@gmail.com>
2022-04-07 23:26:39 +0200

    aggiunto pacchetto font awesome vue component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,16 @@
     <header>
         <!-- container -->
         <!-- due div wrapper a sinistra e destra in flex -->
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <p></p>
+                </div>
+                <div class="col-6">
+                    right
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -12,5 +22,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/scss/style.scss';
 
+    header{
+        background-color: $shark;
+        color: $white;
+
+        div.container{
+
+        }
+    }
 </style>
