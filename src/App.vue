9930bb213500136e45ffeaa00f1bfb739bed24d7
rowl <airowl.gasga@gmail.com>
2022-04-07 16:41:26 +0200

    importato font family montserrat

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,17 @@
     <Footer />
 
     <div id="try">
-      
+      <h1>Lorem Ipsum H1</h1>
+      <h2>Lorem Ipsum H2</h2>
+      <h5>Lorem Ipsum H5</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tenetur sed autem dolorem harum dignissimos natus assumenda illo labore nulla cumque, nam inventore, consectetur, facilis perferendis quia cupiditate libero ut.</p>
+      <p>Uppercase Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tenetur sed autem dolorem harum dignissimos natus assumenda illo labore nulla cumque, nam inventore, consectetur, facilis perferendis quia cupiditate libero ut.</p>
+      <a href="#">
+        Lorem Ipsum
+      </a>
+      <a href="#">
+        Lorem Ipsum uppercase
+      </a>
     </div>
   </div>
 </template>
