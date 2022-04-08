bc13f1839adb79a65d20bc5975e08ef689021bd7
rowl <airowl.gasga@gmail.com>
2022-04-08 12:50:00 +0200

    completato components section companyintro

--- src/components/main-elements/CompanyIntro.vue
+++ src/components/main-elements/CompanyIntro.vue
@@ -1,6 +1,9 @@
 <template>
     <section id="company-intro">
-
+        <p class="uppercase">introducing video</p>
+        <h2>Advisory <span class="shown text-blue-lagoon">Performance</span></h2>
+        <p class="text-grey">Financial executives need to review their strategies, operating models and capabilities so that their <br> areas can generate sound information for better decision making.</p>
+        <img src="../../assets/images/video-1.jpg" alt="Introduction Video">
     </section>
 </template>
 
@@ -11,5 +14,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+
+section#company-intro{
+    text-align: center;
+    padding: 8rem;
+
+    & *:not(:last-child){
+        margin-bottom: 2rem;
+    }
+
+    img{
+        border-radius: .2rem;
+        height: 30rem;
+        margin-top: 1rem;
+    }    
+}
 
 </style>
