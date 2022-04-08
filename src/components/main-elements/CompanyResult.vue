33e15a7ca7f4d40a43279abad7fa8f836b67fa01
rowl <airowl.gasga@gmail.com>
2022-04-08 14:17:31 +0200

    completato components section company result

--- src/components/main-elements/CompanyResult.vue
+++ src/components/main-elements/CompanyResult.vue
@@ -1,15 +1,89 @@
 <template>
     <section id="company-result">
-
+        <img src="../../assets/images/city.jpg" alt="Background City">
+        <div class="overlay"></div>
+        <div class="company-result-contents">
+            <p class="uppercase">what are we doing</p>
+            <h2 class="text-white"><span class="shown">Result</span> in Numbers</h2>
+            <div class="container">
+                <ul>
+                    <li v-for="(element, index) in dataResults" :key="index">
+                        <p class="number">{{element.number}}</p>
+                        <h3 class="text-white">{{element.text}}</h3>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
     </section>
 </template>
 
 <script>
 export default {
-    name: 'sectionCompanyResult'
+    name: 'sectionCompanyResult',
+    data: function(){
+        return{
+            dataResults: [
+                {
+                    number: 128,
+                    text: 'Certifications'
+                },
+                {
+                    number: 230,
+                    text: 'Employees'
+                },
+                {
+                    number: 517,
+                    text: 'Customers'
+                },
+                {
+                    number: 94,
+                    text: 'Countries Served'
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+
+section#company-result{
+    text-align: center;
+    position: relative;
+    overflow: hidden;
+    padding: 8rem;
+
+    img{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        filter: grayscale(100%);
+    }
+
+    div.company-result-contents{
+        position: relative;
+        z-index: 2;
+
+        h2{
+            margin-top: .8rem;
+        }
+
+        ul{
+            @include flex(row, space-between, center);
+            margin-top: 6rem;
+
+            p.number{
+                font-size: 1.5rem;
+                color: $fountain-blue;
+                font-weight: 600;
+                margin-bottom: .7rem;
+            }
+        }
+    }
+
+}
 
 </style>
