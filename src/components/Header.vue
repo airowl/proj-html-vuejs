9a3527bcddbdd68a2757ed82393b8c3b1a6c607a
rowl <airowl.gasga@gmail.com>
2022-04-08 10:37:17 +0200

    completato components header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,15 +5,23 @@
         <div class="container">
             <div class="row">
                 <div class="col-6">
-                    <font-awesome-icon icon="fa-solid fa-clock" />
-                    <font-awesome-icon icon="fa-solid fa-phone" />
-                    <font-awesome-icon icon="fa-solid fa-envelope" />
-                    <font-awesome-icon icon="fa-brands fa-facebook-f" />
-                    <font-awesome-icon icon="fa-brands fa-twitter" />
-                    <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+                    <p><font-awesome-icon icon="fa-solid fa-clock" />  Open Hours: Mon - Sat - 9:00 - 18:00</p>
                 </div>
-                <div class="col-6">
-                    right
+                <div class="col-6" id="header-right-side">
+                    <p>
+                        <font-awesome-icon icon="fa-solid fa-phone" />  
+                        +1 (305) 1234-5678
+                        </p>
+                    <p>
+                        <font-awesome-icon icon="fa-solid fa-envelope" />  
+                        hello@example.com
+                    </p>
+                    <div class="header-social-links">
+                        <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                        <span><font-awesome-icon icon="fa-brands fa-twitter" /></span>
+                        <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+                    </div>
+                    
                 </div>
             </div>
         </div>
@@ -34,7 +42,23 @@ export default {
         color: $white;
 
         div.container{
+            
+            div.row{
+                @include flex(row, space-between, center);
+                padding: .8rem 0;
 
+                div#header-right-side{
+                    @include flex(row, center, center);
+
+                    > p {
+                        margin-right: 1.75rem;
+                    }
+                    
+                    .header-social-links > span{
+                        margin: 0 2rem;
+                    }
+                }
+            }
         }
     }
 </style>
