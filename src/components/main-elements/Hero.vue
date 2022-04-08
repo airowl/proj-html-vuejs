ec3f3a6e2038b11e16c46b10ec312f8b11ac902c
rowl <airowl.gasga@gmail.com>
2022-04-08 10:48:56 +0200

    nel components hero inserito bg image

--- src/components/main-elements/Hero.vue
+++ src/components/main-elements/Hero.vue
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+    section#hero{
+        background-image: url(../../assets/images/bg-2.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 100vw;
+        height: calc(100vh - 46.6px);
+    }
 
 </style>
