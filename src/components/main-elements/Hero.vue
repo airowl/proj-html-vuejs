e6d2e6325797022e5046fc8ff4e2cdcadcc9dd4d
rowl <airowl.gasga@gmail.com>
2022-04-08 11:20:29 +0200

    inserito elementi all'interno della hero

--- src/components/main-elements/Hero.vue
+++ src/components/main-elements/Hero.vue
@@ -1,12 +1,60 @@
 <template>
     <section id="hero">
+        <div class="overlay"></div> 
+        <!-- header -->
+        <header>
+            <div class="container">
+                <nav class="row">
+                    <div class="col-5">
+                        <img src="../../assets/images/nexgen-logo.svg" alt="Logo SVG">
+                    </div>
+                    <ul class="col-7">
+                        
+                        <li>
+                            <button class="full">
+                                <a href="#" class="text-white">Get in touch</a>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <!-- hero content -->
+        <div class="hero-content">
 
+        </div>
     </section>
 </template>
 
 <script>
 export default {
-    name: 'sectionHero'
+    name: 'sectionHero',
+    data: function(){
+        return{
+            navBarLinks: [
+                {
+                    text: 'home',
+                    url: '#'
+                },
+                {
+                    text: 'about',
+                    url: '#'
+                },
+                {
+                    text: 'services',
+                    url: '#'
+                },
+                {
+                    text: 'team',
+                    url: '#'
+                },
+                {
+                    text: 'blog',
+                    url: '#'
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -18,6 +66,23 @@ export default {
         background-repeat: no-repeat;
         width: 100vw;
         height: calc(100vh - 46.6px);
+        color: $white;
+        position: relative;
+
+        header{
+            position: relative;
+            z-index: 1;
+
+            nav.row{
+                @include flex(row, space-between, center);
+
+                img{
+                    height: 2rem;
+                }
+
+                
+            }
+        }
     }
 
 </style>
