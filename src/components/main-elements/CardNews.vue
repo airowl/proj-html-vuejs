0ccf143334a5cb1656199a49e17b73666086b7a6
rowl <airowl.gasga@gmail.com>
2022-04-09 18:53:49 +0200

    inserito elementi nel component section latestnews

---
+++ src/components/main-elements/CardNews.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="card-news">
+        <img src="../../assets/images/news-1.jpg" alt="">
+        <div class="overlay"></div>
+        <h3>Increasing creativity is possible for everyone</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cardNews'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+
+    div#card-news{
+        color: $white;
+        position: relative;
+        width: calc((100% / 3) - 1rem);
+        height: 23rem;
+        border-radius: 1rem;
+        overflow: hidden;
+
+        img{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            z-index: 0;
+        }
+
+        h3{
+            position: absolute;
+            z-index: 2;
+            bottom: 2.5rem;
+            font-size: 1.5rem;
+            text-align: center;
+        }
+    }
+
+</style>
--- src/components/main-elements/LatestNews.vue
+++ src/components/main-elements/LatestNews.vue
@@ -1,15 +1,72 @@
 <template>
     <section id="latest-news">
+        <div class="container">
 
+            <div id="latest-news-text">
+                <p class="uppercase">our editorial content</p>
+                <h2>
+                    Latest 
+                    <span class="shown">News</span>
+                </h2>
+
+                <div class="row">
+                    <div>
+                        <p>
+                            Every week we publish content about what is best in the business world.
+                        </p>
+                    </div>
+                    <div>
+                        <a href="#" class="btn full uppercase">
+                            see all
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="gallery-news">
+                <CardNews 
+                
+                />
+            </div>
+
+        </div>
     </section>
 </template>
 
 <script>
+import CardNews from './CardNews.vue';
+
 export default {
-    name: 'sectionLatestNews'
+    name: 'sectionLatestNews',
+    components: {
+        CardNews
+    },
+    
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
+
+    section#latest-news{
+        background-color: rgba($silver-sand, $alpha: .2);
+        padding: 5rem;
+
+        div#latest-news-text{
+            margin-bottom: 2rem;
+                
+            p{
+                margin-bottom: 1rem;
+            }
+
+            h2{
+                margin-bottom: 2rem;
+            }
+
+            div.row{
+                @include flex(row, space-between, center)
+            }
+        }
+    }
 
 </style>
