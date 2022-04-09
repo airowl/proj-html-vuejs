25f22be68fba165e8b0a88e6fb7cdb4c7cd3fd90
rowl <airowl.gasga@gmail.com>
2022-04-09 13:50:05 +0200

    section team creato array di oggetti riguardo i dati di ogni singolo membro

--- src/components/main-elements/Team.vue
+++ src/components/main-elements/Team.vue
@@ -6,10 +6,97 @@
 
 <script>
 export default {
-    name: 'sectionTeam'
+    name: 'sectionTeam',
+    data: function(){
+        return{
+            teamMembers: [
+                {
+                    profilePhoto: '../../assets/images/team-1.jpg',
+                    firstName: 'david',
+                    lastName: 'cooper',
+                    role: 'cto & co-founder',
+                    socialLinks: [
+                        {
+                            socialName: 'Facebook',
+                            urlProfile: '#'
+                        },
+                        {
+                            socialName: 'Twitter',
+                            urlProfile: ''
+                        },
+                        {
+                            socialName: 'Linked-In',
+                            urlProfile: '#'
+                        }
+                    ]
+                },
+                {
+                    profilePhoto: '../../assets/images/team-2.jpg',
+                    firstName: 'oliver',
+                    lastName: 'jones',
+                    role: 'chief procurement',
+                    socialLinks: [
+                        {
+                            socialName: 'Facebook',
+                            urlProfile: '#'
+                        },
+                        {
+                            socialName: 'Twitter',
+                            urlProfile: ''
+                        },
+                        {
+                            socialName: 'Linked-In',
+                            urlProfile: '#'
+                        }
+                    ]
+                },
+                {
+                    profilePhoto: '../../assets/images/team-3.jpg',
+                    firstName: 'emma',
+                    lastName: 'lopez',
+                    role: 'chief marketing',
+                    socialLinks: [
+                        {
+                            socialName: 'Facebook',
+                            urlProfile: '#'
+                        },
+                        {
+                            socialName: 'Twitter',
+                            urlProfile: ''
+                        },
+                        {
+                            socialName: 'Linked-In',
+                            urlProfile: '#'
+                        }
+                    ]
+                },
+                {
+                    profilePhoto: '../../assets/images/team-4.jpg',
+                    firstName: 't.',
+                    lastName: 'johnson',
+                    role: 'ceo & president',
+                    socialLinks: [
+                        {
+                            socialName: 'Facebook',
+                            urlProfile: '#'
+                        },
+                        {
+                            socialName: 'Twitter',
+                            urlProfile: ''
+                        },
+                        {
+                            socialName: 'Linked-In',
+                            urlProfile: '#'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/style.scss';
 
 </style>
