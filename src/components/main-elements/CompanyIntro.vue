e3b6386fab8da902bba3c0e99c118b7773043957
rowl <airowl.gasga@gmail.com>
2022-04-11 14:26:47 +0200

    refactoring component company intro e piccole modifiche per scss globals

--- src/components/main-elements/CompanyIntro.vue
+++ src/components/main-elements/CompanyIntro.vue
@@ -1,9 +1,20 @@
 <template>
     <section id="company-intro">
-        <p class="uppercase">introducing video</p>
-        <h2>Advisory <span class="shown text-blue-lagoon">Performance</span></h2>
-        <p class="text-grey">Financial executives need to review their strategies, operating models and capabilities so that their <br> areas can generate sound information for better decision making.</p>
+
+        <p class="uppercase">
+            introducing video
+        </p>
+
+        <h2>Advisory 
+            <span class="shown text-blue-lagoon">Performance</span>
+        </h2>
+
+        <p>
+            Financial executives need to review their strategies, operating models and capabilities so that their <br> areas can generate sound information for better decision making.
+        </p>
+
         <img src="../../assets/images/video-1.jpg" alt="Introduction Video">
+
     </section>
 </template>
 
@@ -16,19 +27,19 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/scss/style.scss';
 
-section#company-intro{
-    text-align: center;
-    padding: 8rem;
+    section#company-intro{
+        text-align: center;
+        padding: 8rem;
 
-    & *:not(:last-child){
-        margin-bottom: 2rem;
+        & *:not(:last-child){
+            margin-bottom: 2rem;
+        }
+
+        img{
+            border-radius: .2rem;
+            height: 30rem;
+            margin-top: 1rem;
+        }    
     }
-
-    img{
-        border-radius: .2rem;
-        height: 30rem;
-        margin-top: 1rem;
-    }    
-}
 
 </style>
