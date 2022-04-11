4b40861c9c376eeeea59e7551ea24539c625448c
rowl <airowl.gasga@gmail.com>
2022-04-11 15:17:02 +0200

    refactoring component newsletter

--- src/components/main-elements/NewsLetter.vue
+++ src/components/main-elements/NewsLetter.vue
@@ -2,28 +2,36 @@
     <section id="news-letter">
         <div class="container">
             <div class="row">
-                <div class="col-xs-6">
-                    <div id="news-letter-heading" class="heading-without-btn">
 
+                <div class="col-xs-6">
+
+                    <div id="news-letter-heading" class="heading-without-btn">
                         <p class="uppercase">newsletter</p>
+
                         <h2 class="text-white">
                             <span class="shown">Know</span> 
                             First
                         </h2>
+
                         <p class="description text-white">
                             Follow closely and receive content about our company and the news of the current market.
                         </p>
-                        
                     </div>
+
                 </div>
+
                 <div class="col-xs-6">
+
                     <form action="">
                         <input class="input-style" type="text" placeholder="Name" name="firstname">
+                        
                         <input class="input-style" type="email" placeholder="Email" name="email">
+
                         <button>
                             subscribe
                         </button>
                     </form>
+
                 </div>
             </div>
         </div>
